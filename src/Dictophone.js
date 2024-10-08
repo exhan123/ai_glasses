4cf30bcc20a7e0e40647ea3429b8dade75eb7923
EMoney6969 <114787700+EMoney6969@users.noreply.github.com>
2024-10-08 11:45:08 -0500

    new class

--- src/Dictophone.js
+++ src/Dictophone.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
-
 
 const Dictaphone = () => {
   const {
@@ -30,17 +24,3 @@ const Dictaphone = () => {
   );
 };
 export default Dictaphone;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Dictaphone />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-
